10ccd9b5bbbbaac52d660d0f20c047098dda74b1
Juan Zambrano <jpablozambrano@hotmail.com>
2023-09-19 15:57:07 -0600

    update ejer1rec
    Now it runs several times

--- Act_Programacion_dinamica/ejer1Rec.cpp
+++ Act_Programacion_dinamica/ejer1Rec.cpp
@@ -1,5 +1,5 @@
 /**
- * Actividad: Programación dinámica, backtracking, y ramificación y poda
+ * Actividad Ejemplos de "divide y vencerás" y algoritmos avaros
  * Ejercicio 1 - varilla recursiva
  * Hecho por el Alumno:
  * Juan Pablo Zambrano Barajas A01636420
@@ -15,35 +15,36 @@
 #include <vector>
 using namespace std;
 
-long long maxEarning = 0;
 const vector<int> rodPrices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
 
-int cut(const vector<int> &prices, int lenght) {
-  // Base case
-  if (lenght == 0) {
+int cut(const vector<int> &prices, int length) {
+  if (length == 0) {
     return 0;
   }
   int maxValue = -2147483648;
-  long long costo = 0;
-  for (int i = 1; i <= lenght; i++) {
-    int tmpPrecio = 0;
-    if (i - 1 < prices.size()) {
-      tmpPrecio = prices[i - 1];
-    }
-    maxValue = max(tmpPrecio + cut(prices, lenght - i), maxValue);
+  for (int i = 1; i <= length; i++) {
+    int tmpPrice = (i - 1 < prices.size()) ? prices[i - 1] : 0;
+    maxValue = max(maxValue, tmpPrice + cut(prices, length - i));
   }
   return maxValue;
 }
 
 int main() {
-  int length = 20;
-  auto start = std::chrono::high_resolution_clock::now();
-  maxEarning = cut(rodPrices, length);
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  vector<int> rodLengths = {13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+                            23, 24, 25, 30};
 
-  cout << "La varilla de " << length
-       << " de longitud, nos da de ganancia: " << maxEarning << endl;
-  cout << "Tiempo de ejecución: " << duration.count() << " microsegundos" << endl;
+  for (int length : rodLengths) {
+    auto start = chrono::high_resolution_clock::now();
+    int maxEarning = cut(rodPrices, length);
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration =
+        chrono::duration_cast<chrono::microseconds>(stop - start);
+
+    cout << "La varilla de " << length
+         << " de longitud, nos da de ganancia: " << maxEarning << endl;
+    cout << "Tiempo de ejecución: " << duration.count() << " microsegundos"
+         << endl;
+  }
+
   return 0;
 }
