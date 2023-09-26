d33b5568b48bfe3ff81d0f0d1e7b202c2862812c
Juan Zambrano <jpablozambrano@hotmail.com>
2023-09-26 16:18:53 -0600

    prhf

--- Act_busq_patr_hash/ejer2.cpp
+++ Act_busq_patr_hash/ejer2.cpp
@@ -4,7 +4,9 @@
 #include <string>
 #include <vector>
 
-using namespace std; // Add this line to use the std namespace
+using namespace std; 
+const int prime = 47;
+const int largeModPrime = 1e9 + 9; //another prime num ber
 
 // Function to read a text file and insert words into a vector
 vector<string> fillVectorWithWords(const string& filename) {
@@ -30,22 +32,30 @@ vector<string> fillVectorWithWords(const string& filename) {
     return words;
 }
 
-/*
 long long pollynomialRollingHash(const string& word){
-
-}*/
+    long long hashValue = 0;
+    long long power = 1;
+    for(char c: word){
+        hashValue = (hashValue+(c-'a'+1)*power)%largeModPrime;
+        power=(power*prime)%largeModPrime;
+    }
+    return hashValue;
+}
 
 int main() {
+    long long hashV1 = pollynomialRollingHash("ejemplo");
+    long long hashV2 = pollynomialRollingHash("ejemplo");
+    long long hashV3 = pollynomialRollingHash("ejempla");
+    cout << hashV1 << endl;
+    cout << hashV2 << endl;
+    cout << hashV3 << endl;
     vector<string> words = fillVectorWithWords("test.txt");
-
-    if (words.empty()) {
-        cout << "No words found in the file." << endl;
-    } else {
-        cout << "Words in the file:" << endl;
-        for (const string& word : words) {
-            cout << word << endl;
-        }
+    /*
+    for (const string& word : words) {
+        cout << word << endl;
     }
+    */
+    
 
     return 0;
 }
