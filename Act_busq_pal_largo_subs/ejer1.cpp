03a9cf2c6e576ec66443c7f2cda7a606fbd7a0e2
Juan Zambrano <jpablozambrano@hotmail.com>
2023-10-03 23:12:09 -0600

    ejer1

--- Act_busq_pal_largo_subs/ejer1.cpp
+++ Act_busq_pal_largo_subs/ejer1.cpp
@@ -1,29 +1,62 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <sstream>
-
 using namespace std;
 
-string preprocess(string text){
-    string c = "|";
-    string processedString = "";
-    for(int i = 0; i<text.length();i++){
-        c = c + text[i] + "|";
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+string preprocessString(const string& text) {
+    string T = "|";
+    
+    for (int i = 0; i < text.length(); i++) {
+        T = T + text[i] + "|";
     }
-    return processedString;
+    
+    return T;
 }
-string manacher(string file){
-    int center = 0;
-    int left = 0;
-    int right = 0;
-    string palindrome = "";
-    string charStringFile = preprocess(file);
-    for(char c: charStringFile){
-        cout<<c;
+
+string manacher(string text){
+    //Centro del palodromo más a la derecha, la derecha del palindromo más a la derecha, y a la izquierda
+    int center = 0, right = 0;
+    int rl = 1, m = 0, pl = 0, j = 0;
+
+    string T = preprocessString(text);
+    string palindromo = "";
+    string ans = "";
+
+    for(int i = 0; i<text.length();i++){
+        T = T + text[i] + "|";
     }
-    return charStringFile;
-};
+    int N = T.length();
+    vector<int> palindromes(N,0);
+    for(int i = 0;i < N;i++){
+
+        if(i < right){
+            j = 2 * center - i;
+            palindromes[i] = min(right - i, palindromes[j]);
+            
+        }
+        rl = palindromes[i]+1;
+        while(tolower(T[i-rl]) == tolower(T[i+rl]) && i-rl>=0 && i+rl<N ){
+            palindromes[i]++;
+            rl++;
+        }
+        if(palindromes[i] > m){
+            pl = i;
+            m = palindromes[i];
+        }
+        if(i + palindromes[i] > right){
+            right = i + palindromes[i];
+            center = i;
+        }   
+    }
+    for(int i = 0;i<m*2+1;i++){
+        if ( T[pl-m+i] != '|')
+            ans += T[pl-m+i];
+    }
+    return ans;
+}
 
 int main(){
     ifstream file("metamorphosis.txt");
@@ -41,9 +74,8 @@ int main(){
         }
     }
 
-    file.close();  // Close the file
-    manacher(metarmophosis);
-    //cout << "File content:\n" << metarmophosis << std::endl;
+    file.close();  
+    cout<<manacher(metarmophosis)<<endl;
 
-    return 0;  // Exit successfully
+    return 0;  
 }
