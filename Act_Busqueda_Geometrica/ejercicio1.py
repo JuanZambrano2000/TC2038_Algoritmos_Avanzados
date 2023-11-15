d6ab2cb67907954d23ae9ee0129543d512cbba73
Juan Zambrano <jpablozambrano@hotmail.com>
2023-11-15 00:34:05 -0600

    ejercicio1 finished

--- Act_Busqueda_Geometrica/ejercicio1.py
+++ Act_Busqueda_Geometrica/ejercicio1.py
@@ -1,3 +1,5 @@
+import random # To generate random values
+
 class RangeTreeNode:
     def __init__(self, low, high, value, left=None, right=None):
         self.low = low
@@ -53,12 +55,18 @@ def build_range_tree(values):
     right_node = build_range_tree(right_values)
 
     return RangeTreeNode(min(values), max(values), median_value, left_node, right_node)
-
+'''
 values = [5, 2, 4, 1, 3]
+
 range_tree = build_range_tree(values)
 
-split_node = range_tree.find_split_node(6)
-print("Split node value:", split_node.value)
-
 values_in_range = range_tree.values_in_range(2, 6)
-print("Values in range 2-4:", values_in_range)
+print("Values in range 2-6:", values_in_range)
+'''
+
+random_values = [random.randint(-10, 10) for _ in range(2000)]
+range_tree = build_range_tree(random_values)
+
+values_in_range = [(-1,-0.5),(2,4),(-5,-4),(-10,-9.3),(9,9.5)]
+for i in range(len(values_in_range)):
+    print("Values in range", values_in_range[i], ":", range_tree.values_in_range(values_in_range[i][0], values_in_range[i][1]))
